68a7c0b2f6afb1136ba09369e5111ccf9cffede5
Tracy <gl.register1@gmail.com>
2018-04-23 12:17:59 +0800

    update script for using a new tree model with balanced classes

--- DataAnaysis/src/c50_tree_tunning.R
+++ DataAnaysis/src/c50_tree_tunning.R
@@ -6,28 +6,33 @@ source("src/lib-setup.R")
 require("C50")
 require("partykit")
 
+# load data
+load("data/20180420_mod_C50_8_ReadTree.RData")
+
 # plot without preprocess to better visualize the graph
 
-# comment out as model has already trained. 
-# set.seed(1)
-# mod_c50_5 <- C5.0(x = trainingSet%>%select(-Class) %>% mutate(hasReadme=as.factor(hasReadme)) %>% select(-parent),
-#      y=trainingSet$Class,
-#      trials = 50,
-#      rules=FALSE,
-#      winnow = "FALSE")
-# confusionMatrix(predict(mod_c50_5,newdata = trainingSet%>%select(-Class) %>% mutate(hasReadme=as.factor(hasReadme)) %>% select(-parent)),trainingSet$Class,mode=c('everything'))
-# confusionMatrix(predict(mod_c50_5,newdata = data_two_class_test%>%select(-Class) %>% mutate(hasReadme=as.factor(hasReadme)) %>% select(-parent)),data_two_class_test$Class,mode=c('everything'))
-# saveRDS(mod_c50_5 ,file="data/20180420_mod_c50_5.RDS")
+# model info: 
+	# model 8, prepare the final tree model. we can use all the trainingSet_bl for training
+	# to better visualize the graph, the model is trained (1) without preprocess, (2) on balanced set, 
+# comment out as model has already trained. 	
+#set.seed(9237)
+#trainingSet_bl <- upSample(x=trainingSet%>%select(-Class),y=trainingSet$Class)
+#set.seed(1)
+#mod_c50_8 <- C5.0(x = trainingSet_bl%>%select(-Class) %>% mutate(hasReadme=as.factor(hasReadme)) %>% select(-parent),
+#     trainingSet_bl$Class,
+#     trials = 50,
+#     rules=FALSE,
+#     winnow = "FALSE")
+#confusionMatrix(predict(mod_c50_8,newdata = data_two_class_test%>%select(-Class) %>% mutate(hasReadme=as.factor(hasReadme)) #%>% select(-parent)),data_two_class_test$Class,mode=c('everything'))
+
 
 
 # for plotting the tree
-mod_c50_5 <- readRDS(file="data/20180420_mod_c50_5.RDS")
-pdf(file="data/20180420_mod_c50_5.pdf", width=100/2.54, height=60/2.54)
+pdf(file="data/20180420_mod_c50_8_Tree.pdf", width=100/2.54, height=60/2.54)
 # plot the whole tree
-plot(mod_c50_5)
-myTree2 <- C50:::as.party.C5.0(mod_c50_5)
+plot(mod_c50_8)
+myTree2 <- C50:::as.party.C5.0(mod_c50_8)
 # plot the partial tree by specifying the node number, i.e., 217
 plot(myTree2[217])
 dev.off()
 
-pdf(file="data/20180420_mod_c50_5_b.pdf", width=100/2.54, height=60/2.54)
